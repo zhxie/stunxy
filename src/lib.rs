250e1f38139dcbc1bd5fb719bd44e9cdd3501917
Xie Zhihao <xzh1206@gmail.com>
2020-07-20 00:56:53 +0800

    Implement STUN

---
+++ src/lib.rs
@@ -0,0 +1,250 @@
+//! Deal with NAT traversal using STUN.
+
+use bytes::BytesMut;
+use rand::Rng;
+use socks::{Socks5Datagram, TargetAddr};
+use std::io::{Error, ErrorKind, Result};
+use std::net::{SocketAddr, UdpSocket};
+use std::time::Duration;
+use stun3489::codec::{self, BindRequest, ChangeRequest, Request};
+
+/// Represents an socket which can send data to and receive data from a certain address.
+pub trait RW: Send + Sync {
+    /// Returns the socket address that this socket was created from.
+    fn local_addr(&self) -> Result<SocketAddr>;
+
+    /// Sends data on the socket to the given address.
+    fn send_to(&self, buf: &[u8], addr: SocketAddr) -> Result<usize>;
+
+    /// Receives a single datagram message on the socket.
+    fn recv_from(&self, buf: &mut [u8]) -> Result<(usize, SocketAddr)>;
+
+    /// Sets the read timeout to the timeout specified.
+    fn set_read_timeout(&self, dur: Option<Duration>) -> Result<()>;
+
+    /// Sets the write timeout to the timeout specified.
+    fn set_write_timeout(&self, dur: Option<Duration>) -> Result<()>;
+
+    /// Returns the read timeout of this socket.
+    fn read_timeout(&self) -> Result<Option<Duration>>;
+
+    /// Returns the write timeout of this socket.
+    fn write_timeout(&self) -> Result<Option<Duration>>;
+}
+
+/// Represents an UDP datagram, containing a TCP stream keeping the SOCKS proxy alive and an UDP
+/// socket sending and receiving data.
+#[derive(Debug)]
+pub struct Datagram {
+    datagram: Socks5Datagram,
+}
+
+impl Datagram {
+    /// Creates a new `Datagram`.
+    pub fn bind(proxy: SocketAddr, addr: SocketAddr) -> Result<Datagram> {
+        let datagram = Socks5Datagram::bind(proxy, addr)?;
+
+        Ok(Datagram { datagram })
+    }
+}
+
+impl RW for Datagram {
+    fn local_addr(&self) -> Result<SocketAddr> {
+        let addr = self.datagram.get_ref().local_addr()?;
+
+        Ok(addr)
+    }
+
+    fn send_to(&self, buf: &[u8], addr: SocketAddr) -> Result<usize> {
+        let size = self.datagram.send_to(buf, addr)?;
+
+        Ok(size)
+    }
+
+    fn recv_from(&self, buf: &mut [u8]) -> Result<(usize, SocketAddr)> {
+        let (size, addr) = self.datagram.recv_from(buf)?;
+
+        return match addr {
+            TargetAddr::Ip(addr) => Ok((size, addr)),
+            _ => unreachable!(),
+        };
+    }
+
+    fn set_read_timeout(&self, dur: Option<Duration>) -> Result<()> {
+        self.datagram.get_ref().set_read_timeout(dur)?;
+
+        Ok(())
+    }
+
+    fn set_write_timeout(&self, dur: Option<Duration>) -> Result<()> {
+        self.datagram.get_ref().set_write_timeout(dur)?;
+
+        Ok(())
+    }
+
+    fn read_timeout(&self) -> Result<Option<Duration>> {
+        let duration = self.datagram.get_ref().read_timeout()?;
+
+        Ok(duration)
+    }
+
+    fn write_timeout(&self) -> Result<Option<Duration>> {
+        let duration = self.datagram.get_ref().write_timeout()?;
+
+        Ok(duration)
+    }
+}
+
+/// Represents an UDP socket.
+#[derive(Debug)]
+pub struct Socket {
+    socket: UdpSocket,
+}
+
+impl Socket {
+    /// Creates a new `Socket`.
+    pub fn bind(addr: SocketAddr) -> Result<Socket> {
+        let socket = UdpSocket::bind(addr)?;
+
+        Ok(Socket { socket })
+    }
+}
+
+impl RW for Socket {
+    fn local_addr(&self) -> Result<SocketAddr> {
+        let addr = self.socket.local_addr()?;
+
+        Ok(addr)
+    }
+
+    fn send_to(&self, buf: &[u8], addr: SocketAddr) -> Result<usize> {
+        let size = self.socket.send_to(buf, addr)?;
+
+        Ok(size)
+    }
+
+    fn recv_from(&self, buf: &mut [u8]) -> Result<(usize, SocketAddr)> {
+        let (size, addr) = self.socket.recv_from(buf)?;
+
+        Ok((size, addr))
+    }
+
+    fn set_read_timeout(&self, dur: Option<Duration>) -> Result<()> {
+        self.socket.set_read_timeout(dur)?;
+
+        Ok(())
+    }
+
+    fn set_write_timeout(&self, dur: Option<Duration>) -> Result<()> {
+        self.socket.set_write_timeout(dur)?;
+
+        Ok(())
+    }
+
+    fn read_timeout(&self) -> Result<Option<Duration>> {
+        let duration = self.socket.read_timeout()?;
+
+        Ok(duration)
+    }
+
+    fn write_timeout(&self) -> Result<Option<Duration>> {
+        let duration = self.socket.write_timeout()?;
+
+        Ok(duration)
+    }
+}
+
+/// Generates a random transaction ID with 64 bits.
+pub fn generate_rand_id() -> u64 {
+    let mut rng = rand::thread_rng();
+    let r: u64 = rng.gen();
+
+    r
+}
+
+/// Represents a response of a STUN request.
+pub struct Response {
+    /// Represents the mapped address.
+    pub mapped_addr: SocketAddr,
+    /// Represents the changed address.
+    pub changed_addr: SocketAddr,
+}
+
+/// Executes a STUN test I.
+pub fn stun_test_1(rw: &Box<dyn RW>, addr: SocketAddr, id: u64) -> Result<Response> {
+    // Request echo from same address, same port
+    let req = BindRequest {
+        response_address: None,
+        change_request: ChangeRequest::None,
+        username: None,
+    };
+
+    stun_test(rw, addr, id, Request::Bind(req))
+}
+
+/// Executes a STUN test II.
+pub fn stun_test_2(rw: &Box<dyn RW>, addr: SocketAddr, id: u64) -> Result<Response> {
+    // Request echo from different address, different port
+    let req = BindRequest {
+        response_address: None,
+        change_request: ChangeRequest::IpAndPort,
+        username: None,
+    };
+
+    stun_test(rw, addr, id, Request::Bind(req))
+}
+
+/// Executes a STUN test III.
+pub fn stun_test_3(rw: &Box<dyn RW>, addr: SocketAddr, id: u64) -> Result<Response> {
+    // Request echo from same address, different port
+    let req = BindRequest {
+        response_address: None,
+        change_request: ChangeRequest::Port,
+        username: None,
+    };
+
+    stun_test(rw, addr, id, Request::Bind(req))
+}
+
+fn stun_test(rw: &Box<dyn RW>, addr: SocketAddr, id: u64, req: Request) -> Result<Response> {
+    // Encode
+    let mut buffer = BytesMut::new();
+    stun3489::codec::StunCodec::encode((id, req), &mut buffer)?;
+    let buffer = buffer.freeze();
+
+    // Send request
+    let mut recv_buffer = vec![0u8; u16::MAX as usize];
+    let _ = rw.send_to(&buffer[..], addr)?;
+
+    // Receive
+    loop {
+        let (size, _) = rw.recv_from(recv_buffer.as_mut_slice())?;
+        if size <= 0 {
+            return Err(Error::from(ErrorKind::UnexpectedEof));
+        } else {
+            if let Some(outer_addr) = parse(&recv_buffer[..size], id)? {
+                return Ok(outer_addr);
+            }
+        }
+    }
+}
+
+fn parse(buffer: &[u8], id: u64) -> Result<Option<Response>> {
+    // Decode
+    let r = stun3489::codec::StunCodec::decode_const(buffer)?;
+    if let None = r {
+        return Ok(None);
+    }
+    let (i, resp) = r.unwrap();
+    if id != i {
+        return Ok(None);
+    }
+
+    // Parse response
+    match resp {
+        codec::Response::Bind(resp) => Ok(Some(Response {
+            mapped_addr: resp.mapped_address,
+            changed_addr: resp.changed_address,
+        })),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,154 @@
+use lib::{Datagram, Socket, RW};
+use std::clone::Clone;
+use std::io::ErrorKind;
+use std::net::{IpAddr, SocketAddr};
+use std::time::Duration;
+use structopt::StructOpt;
+use stunxy as lib;
+
+#[derive(StructOpt, Clone, Debug, Eq, Hash, PartialEq)]
+#[structopt(about)]
+struct Flags {
+    #[structopt(name = "ADDRESS", help = "Server")]
+    pub server: IpAddr,
+    #[structopt(
+        long,
+        short,
+        help = "Port",
+        value_name = "VALUE",
+        default_value = "3478"
+    )]
+    pub port: u16,
+    #[structopt(
+        long = "socks-proxy",
+        short = "s",
+        help = "SOCKS proxy",
+        value_name = "ADDRESS"
+    )]
+    pub proxy: Option<SocketAddr>,
+    #[structopt(
+        long,
+        short = "w",
+        help = "Timeout to wait for each response",
+        value_name = "VALUE",
+        default_value = "1000"
+    )]
+    pub timeout: u64,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Parse arguments
+    let flags = Flags::from_args();
+
+    // Bind socket
+    let local: SocketAddr = match flags.server {
+        IpAddr::V4(_) => "0.0.0.0:0".parse().unwrap(),
+        IpAddr::V6(_) => "[::]:0".parse().unwrap(),
+    };
+    let rw: Box<dyn RW> = match flags.proxy {
+        Some(proxy) => match Datagram::bind(proxy, local) {
+            Ok(datagram) => Box::new(datagram),
+            Err(ref e) => {
+                eprintln!("{}", e);
+                return;
+            }
+        },
+        None => match Socket::bind(local) {
+            Ok(socket) => Box::new(socket),
+            Err(ref e) => {
+                eprintln!("{}", e);
+                return;
+            }
+        },
+    };
+    if flags.timeout != 0 {
+        if let Err(ref e) = rw.set_read_timeout(Some(Duration::from_millis(flags.timeout))) {
+            eprintln!("{}", e);
+            return;
+        }
+    }
+
+    // STUN test I
+    let id = lib::generate_rand_id();
+    let server_addr = SocketAddr::new(flags.server, flags.port);
+    let local_addr = rw.local_addr().unwrap();
+    match lib::stun_test_1(&rw, server_addr, id) {
+        Ok(resp1) => {
+            if resp1.mapped_addr == local_addr {
+                // No NAT, check for firewall
+                // STUN test II
+                match lib::stun_test_2(&rw, server_addr, id) {
+                    Ok(_) => {
+                        println!("Local Address : {}", local_addr);
+                        println!("Remote Address: {}", resp1.mapped_addr);
+                        println!("NAT Type      : Open Internet");
+                    }
+                    Err(ref e) => match e.kind() {
+                        ErrorKind::TimedOut => {
+                            println!("Local Address : {}", local_addr);
+                            println!("Remote Address: {}", resp1.mapped_addr);
+                            println!("NAT Type      : Symmetric Firewall");
+                        }
+                        _ => eprintln!("{}", e),
+                    },
+                }
+            } else {
+                // NAT detected
+                // STUN test II
+                match lib::stun_test_2(&rw, server_addr, id) {
+                    Ok(_) => {
+                        println!("Local Address : {}", local_addr);
+                        println!("Remote Address: {}", resp1.mapped_addr);
+                        println!("NAT Type      : Full-cone NAT");
+                    }
+                    Err(ref e) => match e.kind() {
+                        ErrorKind::TimedOut => {
+                            // STUN test I
+                            match lib::stun_test_3(&rw, resp1.changed_addr, id) {
+                                Ok(resp2) => {
+                                    if resp1.mapped_addr.ip() != resp2.mapped_addr.ip() {
+                                        println!("Local Address : {}", local_addr);
+                                        println!("Remote Address: {}", resp1.mapped_addr);
+                                        println!("NAT Type      : Symmetric NAT");
+                                    } else {
+                                        // STUN test III
+                                        match lib::stun_test_3(&rw, resp1.changed_addr, id) {
+                                            Ok(_) => {
+                                                println!("Local Address : {}", local_addr);
+                                                println!("Remote Address: {}", resp1.mapped_addr);
+                                                println!("NAT Type      : Restricted cone NAT");
+                                            }
+                                            Err(ref e) => match e.kind() {
+                                                ErrorKind::TimedOut => {
+                                                    println!("Local Address : {}", local_addr);
+                                                    println!(
+                                                        "Remote Address: {}",
+                                                        resp1.mapped_addr
+                                                    );
+                                                    println!("NAT Type      : Restricted port NAT");
+                                                }
+                                                _ => eprintln!("{}", e),
+                                            },
+                                        }
+                                    }
+                                }
+                                Err(ref e) => eprintln!("{}", e),
+                            }
+                        }
+                        _ => eprintln!("{}", e),
+                    },
+                }
+            }
+        }
+        Err(ref e) => {
+            // UDP blocked
+            match e.kind() {
+                ErrorKind::TimedOut => {
+                    println!("Local Address: {}", local_addr);
+                    println!("NAT Type     : UDP blocked");
+                }
+                _ => eprintln!("{}", e),
+            }
+        }
+    }
 }
